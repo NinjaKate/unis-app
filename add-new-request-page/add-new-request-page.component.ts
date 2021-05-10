d2ac0d84ac3d79d796e8940543464bbcad2e4714
NinjaKate <38056303+NinjaKate@users.noreply.github.com>
2021-05-11 02:14:04 +0300

    Update

---
+++ add-new-request-page/add-new-request-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-request-page',
+  templateUrl: './add-new-request-page.component.html',
+  styleUrls: ['./add-new-request-page.component.scss']
+})
+export class AddNewRequestPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
